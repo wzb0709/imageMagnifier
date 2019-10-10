25d2b259be49c5b533a142c9809a06b8de81be71
wzb0709 <948079797@qq.com>
2019-10-10 15:47:49 +0800

    complete

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component,Fragment} from 'react'
 
 import ExampleComponent from 'image-magnifier'
 
 export default class App extends Component {
   render () {
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div style={{width:'80%',margin:'0 auto',textAlign:'center'}}>
+        <div style={{fontWeight:'bold',marginBottom:20}}>这里是标题</div>
+        <div style={{marginBottom:20}}>
+          这里是描述这里是描述这里是描述这里是描述这里是描述这里是描述这里是描述,这里是描述这里是描述这里是描述,这里是描述这里是描述
+        </div>
+        <div style={{display:'flex',justifyContent:'center',marginBottom:20}}>
+          <ExampleComponent
+            url='https://pzyfile.oss-cn-hangzhou.aliyuncs.com//6fec2680ea6311e98e5fc1818ef4e028passport1570604847928.jpg'
+            width={400}
+            style={{marginRight:20}}
+          />
+          <ExampleComponent
+            url='https://pzyfile.oss-cn-hangzhou.aliyuncs.com//6fec2680ea6311e98e5fc1818ef4e028passport1570604847928.jpg'
+            width={400}
+          />
+        </div>
+        <ExampleComponent
+          url='https://pzyfile.oss-cn-hangzhou.aliyuncs.com//6fec2680ea6311e98e5fc1818ef4e028passport1570604847928.jpg'
+          width={600}
+          style={{margin:'0 auto'}}
+        />
       </div>
     )
   }
--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,89 @@
  */
 
 import * as React from 'react'
-
 import styles from './styles.css'
 
-export type Props = { text: string }
+export interface IProps {
+  url: string,
+  width?: number | string,
+  height?: number | string,
+  scale?: number,
+  style?: React.CSSProperties;
+}
 
-export default class ExampleComponent extends React.Component<Props> {
+export interface IState {
+  visible: boolean,
+  left: number,
+  top: number,
+  scale_width: number
+}
+
+export default class ExampleComponent extends React.Component<IProps, IState> {
+
+
+  state = {
+    visible: false,
+    left: 0,
+    top: 0,
+    scale_width: 0,
+  }
+
+  handleGetImageWidth = (e: any) => {
+    const scale = this.props.scale || 3
+    this.setState({scale_width: e.target.width * scale})
+  }
+
+  handleMouseEnter = () => {
+    this.setState({visible: true})
+  }
+
+  handleMouseLeave = () => {
+    this.setState({visible: false})
+  }
+
+  handleMouseMove = (e: any) => {
+    const {offsetX, offsetY} = e.nativeEvent
+    this.setState({
+      left: offsetX,
+      top: offsetY,
+    })
+  }
+
   render() {
-    const {
-      text
-    } = this.props
-
+    const {url, width, height} = this.props
+    const {left, top, scale_width} = this.state
+    const scale: number = this.props.scale || 3
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <>
+        <div
+          className={styles.surrounded_div}
+          style={{
+            ...this.props.style,
+            width: width || "auto",
+            height: height || "auto",
+          }}
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+          onMouseMove={this.handleMouseMove}
+        >
+          <img onLoad={this.handleGetImageWidth} alt='' src={url} className={styles.image}/>
+          {this.state.visible && <div className={styles.magnifier} style={{
+            left: left - 60,
+            top: top - 60,
+          }}>
+            <img
+              alt=''
+              src={url}
+              style={{
+                width: scale_width,
+                position: "absolute",
+                left: 60 - (left * scale),
+                top: 60 - (top * scale)
+              }}
+            />
+          </div>}
+        </div>
+      </>
     )
   }
 }
